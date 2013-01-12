113a7d77d45b02ffe3e499b621eefd5f51ab5261
GiorgosMethe <giorgosmethe@gmail.com>
2013-01-12 22:46:05 +0100

    background removal is ready. now is the time to binirize the image and produce the lines

--- NaoLeague/FeatureExtraction/LineExtraction.cpp
+++ NaoLeague/FeatureExtraction/LineExtraction.cpp
@@ -1,116 +1,75 @@
-#include <stdio.h>
-#include <iostream>
-#include "opencv2/core/core.hpp"
-#include "opencv2/nonfree/features2d.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
+/* This is a standalone program. Pass an image name as the first parameter
+of the program.  Switch between standard and probabilistic Hough transform
+by changing "#if 1" to "#if 0" and back */
+#include <cv.h>
+#include <highgui.h>
+#include <math.h>
 
 using namespace cv;
 
-int main()
+Mat extract_green(Mat image){
+    Mat green = cvCreateMat(image.rows, image.cols, CV_8U);
+    for(int i = 0; i < image.rows; i ++){
+        for(int j = 0; j < image.cols; j ++){
+            (green.data + green.step * i)[j] = max(image.at<cv::Vec3b>(i,j)[1]
+            -image.at<cv::Vec3b>(i,j)[2]/2-image.at<cv::Vec3b>(i,j)[0]/2,0);
+        }
+    }
+    return green;
+}
+
+Mat backgroung_removal(Mat image){
+    Mat green_thr;
+    Mat green = extract_green(image);
+    threshold( green, green_thr, 35, 255,0 );
+
+    // this for loop can be done in the above for loop.
+    // I leave it like this for now.
+    for(int j = 0; j < image.cols; j ++){
+        for(int i = 0; i < image.rows; i ++){
+            if((green_thr.data + green_thr.step * i)[j] > 0){
+                break;
+            }else{
+                image.at<cv::Vec3b>(i,j)[0] = 0;
+                image.at<cv::Vec3b>(i,j)[1] = 0;
+                image.at<cv::Vec3b>(i,j)[2] = 0;
+            }
+        }
+    }
+    return image;
+}
+
+int main(int argc, char** argv)
 {
-    Mat object = imread( "image1.png", CV_LOAD_IMAGE_GRAYSCALE );
-
-    if( !object.data )
-    {
-        std::cout<< "Error reading object " << std::endl;
+    Mat src, dst, color_dst, src_no_backgr;
+    if( argc != 2 || !(src=imread(argv[1], 1)).data)
         return -1;
-    }
+    
+    src_no_backgr = backgroung_removal(src);
+    
+    imshow( "Detected Lines", src );
+    waitKey();
 
-    //Detect the keypoints using SURF Detector
-    int minHessian = 500;
 
-    SurfFeatureDetector detector( minHessian );
-    std::vector<KeyPoint> kp_object;
+    // clock_t startTime = clock();
+    // Canny( src, dst, 50, 200, 3 );
+    // cvtColor( src, color_dst, CV_GRAY2BGR );
 
-    detector.detect( object, kp_object );
+    // vector<Vec4i> lines;
+    // HoughLinesP( dst, lines, 1, CV_PI/300, 10, 10, 20 );
+    // for( size_t i = 0; i < lines.size(); i++ )
+    // {
+    //     line( color_dst, Point(lines[i][0], lines[i][1]),
+    //         Point(lines[i][2], lines[i][3]), Scalar(0,0,255), 1, 8 );
+    // }
 
-    //Calculate descriptors (feature vectors)
-    SurfDescriptorExtractor extractor;
-    Mat des_object;
+    // std::cout << lines.size() << std::endl;
+    // namedWindow( "Source", 1 );
+    // imshow( "Source", src );
 
-    extractor.compute( object, kp_object, des_object );
+    // namedWindow( "Detected Lines", 1 );
+    // imshow( "Detected Lines", color_dst );
 
-    FlannBasedMatcher matcher;
-
-    VideoCapture cap(0);
-
-    namedWindow("Good Matches");
-
-    std::vector<Point2f> obj_corners(4);
-
-    //Get the corners from the object
-    obj_corners[0] = cvPoint(0,0);
-    obj_corners[1] = cvPoint( object.cols, 0 );
-    obj_corners[2] = cvPoint( object.cols, object.rows );
-    obj_corners[3] = cvPoint( 0, object.rows );
-
-    char key = 'a';
-    int framecount = 0;
-    while (key != 27)
-    {
-        Mat frame;
-        cap >> frame;
-
-        if (framecount < 5)
-        {
-            framecount++;
-            continue;
-        }
-
-        Mat des_image, img_matches;
-        std::vector<KeyPoint> kp_image;
-        std::vector<vector<DMatch > > matches;
-        std::vector<DMatch > good_matches;
-        std::vector<Point2f> obj;
-        std::vector<Point2f> scene;
-        std::vector<Point2f> scene_corners(4);
-        Mat H;
-        Mat image;
-
-        cvtColor(frame, image, CV_RGB2GRAY);
-
-        detector.detect( image, kp_image );
-        extractor.compute( image, kp_image, des_image );
-
-        matcher.knnMatch(des_object, des_image, matches, 2);
-
-        for(int i = 0; i < min(des_image.rows-1,(int) matches.size()); i++) //THIS LOOP IS SENSITIVE TO SEGFAULTS
-        {
-            if((matches[i][0].distance < 0.6*(matches[i][1].distance)) && ((int) matches[i].size()<=2 && (int) matches[i].size()>0))
-            {
-                good_matches.push_back(matches[i][0]);
-            }
-        }
-
-        //Draw only "good" matches
-        drawMatches( object, kp_object, image, kp_image, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
-        if (good_matches.size() >= 4)
-        {
-            for( int i = 0; i < good_matches.size(); i++ )
-            {
-                //Get the keypoints from the good matches
-                obj.push_back( kp_object[ good_matches[i].queryIdx ].pt );
-                scene.push_back( kp_image[ good_matches[i].trainIdx ].pt );
-            }
-
-            H = findHomography( obj, scene, CV_RANSAC );
-
-            perspectiveTransform( obj_corners, scene_corners, H);
-
-            //Draw lines between the corners (the mapped object in the scene image )
-            line( img_matches, scene_corners[0] + Point2f( object.cols, 0), scene_corners[1] + Point2f( object.cols, 0), Scalar(0, 255, 0), 4 );
-            line( img_matches, scene_corners[1] + Point2f( object.cols, 0), scene_corners[2] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
-            line( img_matches, scene_corners[2] + Point2f( object.cols, 0), scene_corners[3] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
-            line( img_matches, scene_corners[3] + Point2f( object.cols, 0), scene_corners[0] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
-        }
-
-        //Show detected matches
-        imshow( "Good Matches", img_matches );
-
-        key = waitKey(1);
-    }
+    // waitKey(0);
     return 0;
 }
---
+++ NaoLeague/FeatureExtraction/build/CMakeFiles/LineExtraction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LineExtraction.dir/LineExtraction.o"
+  "LineExtraction.pdb"
+  "LineExtraction"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LineExtraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
