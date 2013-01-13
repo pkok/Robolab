2db0458345827d2e73071fa8c9ce71c188c11bd0
fleischhauf <fleischhauf@hotmail.com>
2013-01-13 20:23:44 +0100

    added code and pseudocode to some functions in ParticleFilter

--- NaoLeague/Localization/ParticleFilter.cpp
+++ NaoLeague/Localization/ParticleFilter.cpp
@@ -8,20 +8,36 @@
 #include "ParticleFilter.h"
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
+
+
+double ParticleFilter::random_uniform ()
+{
+  return ((double) rand())/RAND_MAX;
+}
+
+double ParticleFilter::random_gaussian ()
+{
+  double u = random_uniform(), v = random_uniform();
+  return sqrt(-2*log(u))*cos(2*M_PI*v);
+}
+
 
 ParticleFilter::ParticleFilter(){
 	this->width = 720;
 	this->height = 540;
 	this->particle_head = 0;
 	this->particle_current = 0;
+	create_particles(150);
 }
-ParticleFilter::ParticleFilter(int width, int height){
+ParticleFilter::ParticleFilter(int width, int height,int number_of_particles){
 	this->width = width;
 	this->height = height;
 	this->particle_head = 0;
 	this->particle_current = 0;
+	create_particles(number_of_particles);
 }
 ParticleFilter::~ParticleFilter(){
 
@@ -32,9 +48,27 @@ void ParticleFilter::test() {
 }
 int ParticleFilter::resample() {
 
+	// read particle 0..M
+	// draw with probability alpha w[i]t
+	// add x[i]_t to Xt
 }
-int ParticleFilter::dynamic() {
+int ParticleFilter::dynamic(){
 
+	//for 0..M
+		// pose = sample_sample_motion_model
+		// weight = measurement_model
+
+	//CHECK FOR PARTICLES OUTSIDE THE FIELD
+}
+Particle ParticleFilter::sample_motion_model(OdometryInformation odometry_information,Particle last_pose)
+{
+	last_pose.x = odometry_information.x + random_gaussian();
+	last_pose.y = odometry_information.y + random_gaussian();
+	last_pose.rot = odometry_information.rot + random_gaussian();
+}
+int measurement_model(VisualInformation sensory_information,Particle current_pose,int map)
+{
+	return(0);
 }
 /*
  * creates a number of particles with random poses
--- NaoLeague/Localization/ParticleFilter.h
+++ NaoLeague/Localization/ParticleFilter.h
@@ -6,10 +6,23 @@
  */
 #include "Particle.h"
 
+struct VisualInformation {
+	  int range;
+	  int bearing;
+	};
+	struct OdometryInformation {
+		int x;
+		int y;
+		int rot;
+	};
+
+
+
 
 class ParticleFilter {
 
 public:
+
 	//vars
 	int width, height;
 	Particle* particle_head;
@@ -17,16 +30,31 @@ public:
 
 	//constructors
 	ParticleFilter();
-	ParticleFilter(int width, int height);
+	ParticleFilter(int width, int height,int number_of_particles);
 	virtual ~ParticleFilter();
 
 
 
 	void test();
-	int resample();
+
+
+	//incorporate new odometry information (move and rotate particle)
+	Particle sample_motion_model(OdometryInformation odometry_information,Particle last_pose);
+
+	//weight particles accroding to new sensory information (vision)
+	int measurement_model(VisualInformation sensory_information,Particle current_pose,int map);
+
+	//wander through all particles and update odometry and weight according to visual measurement
 	int dynamic();
+	//reassign particle poses according to weight of particles
+	int resample();
 	//particle interaction
 	int create_particles(int number);
 	int count_particles();
 	int print_particles();
+
+
+	//gaussian noise
+	double random_uniform ();
+	double random_gaussian ();
 };
