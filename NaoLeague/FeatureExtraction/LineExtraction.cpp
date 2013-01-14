f5b1238be8c06ddb7d2306d09a60eb1dc961969a
GiorgosMethe <giorgosmethe@gmail.com>
2013-01-14 15:44:06 +0100

    almost completed, finds T and L line-crosses. X cross need to be done. Cyrcle in the middle of the field causes lots of false negatives.

--- NaoLeague/FeatureExtraction/LineExtraction.cpp
+++ NaoLeague/FeatureExtraction/LineExtraction.cpp
@@ -7,12 +7,72 @@ using namespace cv;
 int GREEN_THRESHOLD = 35;
 int BINARY_TYPE = 0;
 int MAX_BINARY_VALUE = 255;
-double LINE_CONN_THERSHOLD = 0.7;
+double LINE_CONN_THERSHOLD = 3.0;
 double LINE_ANGLE_THERSHOLD = 15.0;
 double YEL_THR_R = 0.4;
 double YEL_THR_B = 0.2;
 double YEL_THR_G = 0.4;
 const double PI  =3.141592653589793238462;
+
+double distance_point_line(int, int, int, int, int, int);
+double int_dis_point_line(int, int, int, int, int, int);
+double angle_bet_lines(int, int, int, int, int, int, int, int);
+double distance_point_point(int, int, int, int);
+vector<Vec3i> find_connections(vector<Vec4i>);
+vector<Vec4i> reduce_lines(vector<Vec4i>);
+Mat remove_background(Mat);
+
+int main(int argc, char** argv)
+{
+    Mat src, dst, color_dst, src_no_backgr;
+    Mat src_line_binary;
+    if( argc != 2 || !(src=imread(argv[1], 1)).data)
+        return -1;
+    
+    Mat e;
+    src.copyTo(e);
+    imshow( "Detected", e );
+    // Measuring performance
+    clock_t startTime = clock();
+
+    src_line_binary = remove_background(src);
+    
+    Canny( src_line_binary, dst, 50, 200, 3 );
+    cvtColor( dst, color_dst, CV_GRAY2BGR );
+
+    vector<Vec4i> lines;
+    HoughLinesP( dst, lines, 1, CV_PI/300, 10, 20, 20 );
+	
+	
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+		line( color_dst, Point(lines[i][0], lines[i][1]),
+		Point(lines[i][2], lines[i][3]), Scalar(0,0,255), 2, 8 );
+    }
+    
+    
+    vector<Vec3i> points;
+	points = find_connections(lines);
+	
+    
+    for( int i = 0; i < points.size(); i++ )
+    {
+		if(points[i][2] == 0){
+			circle(e, Point(points[i][0], points[i][1]), 5, Scalar(0,255,0), 2, 8, 0);
+		}else if(points[i][2] == 1){
+			circle(e, Point(points[i][0], points[i][1]), 5, Scalar(255,0,0), 2, 8, 0);
+		}
+    }
+    
+    std::cout << double( clock() - startTime )*1000 / (double)CLOCKS_PER_SEC<< " ms." << std::endl;
+    std::cout << "number of lines: " << lines.size() << std::endl;
+    std::cout << "number of points: " << points.size() << std::endl;
+
+    namedWindow( "Detected Lines", 1 );
+    imshow( "Detected Lines", e );
+    waitKey(0);
+    return 0;
+}
 
 Mat remove_background(Mat image){
     Mat green = cvCreateMat(image.rows, image.cols, CV_8U);
@@ -32,14 +92,22 @@ Mat remove_background(Mat image){
                 (green.data + green.step * i)[j] = 255;
                 if (justGreen > 34){
                     double r = ((double)image.at<cv::Vec3b>(i,j)[2] / 
-                        ((double)image.at<cv::Vec3b>(i,j)[0]+(double)image.at<cv::Vec3b>(i,j)[1]+(double)image.at<cv::Vec3b>(i,j)[2]));
+                        ((double)image.at<cv::Vec3b>(i,j)[0] +
+                        (double)image.at<cv::Vec3b>(i,j)[1] +
+                        (double)image.at<cv::Vec3b>(i,j)[2]));
                     double g = ((double)image.at<cv::Vec3b>(i,j)[1] / 
-                        ((double)image.at<cv::Vec3b>(i,j)[0]+(double)image.at<cv::Vec3b>(i,j)[1]+(double)image.at<cv::Vec3b>(i,j)[2]));
+                        ((double)image.at<cv::Vec3b>(i,j)[0] +
+                        (double)image.at<cv::Vec3b>(i,j)[1] +
+                        (double)image.at<cv::Vec3b>(i,j)[2]));
                     double b = ((double)image.at<cv::Vec3b>(i,j)[0] / 
-                        ((double)image.at<cv::Vec3b>(i,j)[0]+(double)image.at<cv::Vec3b>(i,j)[1]+(double)image.at<cv::Vec3b>(i,j)[2]));
+                        ((double)image.at<cv::Vec3b>(i,j)[0] +
+                        (double)image.at<cv::Vec3b>(i,j)[1] +
+                        (double)image.at<cv::Vec3b>(i,j)[2]));
                     if(r>0.38 && g>0.38 && b<0.2){
                         (green.data + green.step * i)[j] = 0;
-                    }else if(image.at<cv::Vec3b>(i,j)[2]>230 && image.at<cv::Vec3b>(i,j)[1]>230 && image.at<cv::Vec3b>(i,j)[0]<160){
+                    }else if(image.at<cv::Vec3b>(i,j)[2]>230 && 
+                    image.at<cv::Vec3b>(i,j)[1]>230 && 
+                    image.at<cv::Vec3b>(i,j)[0]<160){
                         (green.data + green.step * i)[j] = 0;
                     }else{
                         (green.data + green.step * i)[j] = 255;
@@ -81,20 +149,76 @@ Mat remove_background(Mat image){
     return image;
 }
 
-double distance_point_line(int cx, int cy, int ax, int ay, int bx, int by){
-
-    double r_numerator = (cx-ax)*(bx-ax) + (cy-ay)*(by-ay);
-    double r_denomenator = (bx-ax)*(bx-ax) + (by-ay)*(by-ay);
-    double r = r_numerator / r_denomenator;
-    double px = ax + r*(bx-ax);
-    double py = ay + r*(by-ay);     
-    double s =  ((ay-cy)*(bx-ax)-(ax-cx)*(by-ay) ) / r_denomenator;
-    double distanceLine;
-
-    distanceLine = fabs(s)*sqrt(r_denomenator);
-
-    return distanceLine;
+double distance_point_point(int ax, int ay, int bx, int by){
+	double dx = (ax-bx);
+	double dy = (ay-by);
+    return sqrt(dx*dx + dy*dy);
 }
+
+double distance_point_line(int x, int y, int x1, int y1, int x2, int y2){
+
+    double A = x - x1;
+	double B = y - y1;
+	double C = x2 - x1;
+	double D = y2 - y1;
+	double dot = A * C + B * D;
+	double len_sq = C * C + D * D;
+	double param = dot / len_sq;
+	 
+	double xx,yy;
+	 
+	if(param < 0)
+	{
+	    xx = x1;
+	    yy = y1;
+	}
+	else if(param > 1)
+	{
+	    xx = x2;
+	    yy = y2;
+	}
+	else
+	{
+	    xx = x1 + param * C;
+	    yy = y1 + param * D;
+	}
+	 
+	double dist = distance_point_point(x,y,xx,yy);
+    return dist;
+}
+
+double int_dis_point_line(int x, int y, int x1, int y1, int x2, int y2){
+
+    double A = x - x1;
+	double B = y - y1;
+	double C = x2 - x1;
+	double D = y2 - y1;
+	double dot = A * C + B * D;
+	double len_sq = C * C + D * D;
+	double param = dot / len_sq;
+	 
+	double xx,yy;
+	 
+	if(param < 0)
+	{
+	    xx = x1;
+	    yy = y1;
+	}
+	else if(param > 1)
+	{
+	    xx = x2;
+	    yy = y2;
+	}
+	else
+	{
+	    xx = x1 + param * C;
+	    yy = y1 + param * D;
+	}
+	 
+	double dist = distance_point_point(x,y,(x1+x2)/2,(y1+y2)/2);
+    return dist;
+}
+
 
 double angle_bet_lines(int a1x, int a1y, int b1x, int b1y, int a2x, int a2y, int b2x, int b2y){
 
@@ -104,50 +228,70 @@ double angle_bet_lines(int a1x, int a1y, int b1x, int b1y, int a2x, int a2y, int
     return abs(res); 
 }
 
-
-int main(int argc, char** argv)
-{
-    Mat src, dst, color_dst, src_no_backgr;
-    Mat src_line_binary;
-    if( argc != 2 || !(src=imread(argv[1], 1)).data)
-        return -1;
-    
-    // Measuring performance
-    clock_t startTime = clock();
-
-    src_line_binary = remove_background(src);
-    
-    Canny( src_line_binary, dst, 50, 200, 3 );
-    cvtColor( dst, color_dst, CV_GRAY2BGR );
-
-    vector<Vec4i> lines;
-    HoughLinesP( dst, lines, 1, CV_PI/300, 10, 20, 20 );
-
-    for( size_t i = 0; i < lines.size(); i++ ){
-        for( size_t j = 0; j < lines.size(); j++ ){
+vector<Vec3i> find_connections(vector<Vec4i> lines){
+	vector<Vec3i> points;
+    for( int i = 0; i < lines.size(); i++ ){
+        for( int j = 0; j < lines.size(); j++ ){
             if(i != j){
-                for( size_t p = 0; p < 2; p++ ){
-                    if(distance_point_line(lines[i][2*p], lines[i][2*p+1], lines[j][0], lines[j][1], lines[j][2], lines[j][3]) < LINE_CONN_THERSHOLD){
-                        if(angle_bet_lines(lines[j][0], lines[j][1], lines[j][2], lines[j][3],lines[i][0], lines[i][1], lines[i][2], lines[i][3]) > LINE_ANGLE_THERSHOLD){
-                            circle(color_dst, Point(lines[i][2*p], lines[i][2*p+1]), 5, Scalar(0,255,0), 1, 8, 0);
-                        }
-                    }
-                }   
+                for( int p = 0; p < 2; p++ ){
+					// point's distance from intersection point is below the thershold
+					if(distance_point_line(lines[i][2*p], lines[i][2*p+1], lines[j][0], lines[j][1], lines[j][2], lines[j][3]) < LINE_CONN_THERSHOLD){
+						// intersection point is near line's start or end
+						if(abs(int_dis_point_line(lines[i][2*p], lines[i][2*p+1], lines[j][0], lines[j][1], lines[j][2], lines[j][3]) -
+							distance_point_point(lines[j][0], lines[j][1], lines[j][2], lines[j][3])/2) < LINE_CONN_THERSHOLD){
+							// angle between the two lines has to be larger than a thershold to not to be considered,
+							// a straight line.
+							if(angle_bet_lines(lines[j][0], lines[j][1], lines[j][2], lines[j][3],lines[i][0], lines[i][1], lines[i][2], lines[i][3]) > LINE_ANGLE_THERSHOLD){
+								points.push_back(Vec3i(lines[i][2*p], lines[i][2*p+1], 0));
+							}
+						}
+						else if(int_dis_point_line(lines[i][2*p], lines[i][2*p+1], lines[j][0], lines[j][1], lines[j][2], lines[j][3]) -
+							distance_point_point(lines[j][0], lines[j][1], lines[j][2], lines[j][3])/2 < -10.0){
+								if(angle_bet_lines(lines[j][0], lines[j][1], lines[j][2], lines[j][3],lines[i][0], lines[i][1], lines[i][2], lines[i][3]) > LINE_ANGLE_THERSHOLD){
+									points.push_back(Vec3i(lines[i][2*p], lines[i][2*p+1], 1));
+								}		
+						}
+					}
+				}  
             }
         }
     }
-
-    for( size_t i = 0; i < lines.size(); i++ )
-    {
-        line( color_dst, Point(lines[i][0], lines[i][1]),
-            Point(lines[i][2], lines[i][3]), Scalar(0,0,255), 1, 8 );
-    }
-    
-    std::cout << double( clock() - startTime )*1000 / (double)CLOCKS_PER_SEC<< " ms." << std::endl;
-    std::cout << lines.size() << std::endl;
-
-    namedWindow( "Detected Lines", 1 );
-    imshow( "Detected Lines", color_dst );
-    waitKey(0);
-    return 0;
+    return points;
+}
+vector<Vec4i> reduce_lines(vector<Vec4i> lines){
+	vector<Vec4i> reduced_lines;
+	for( int i = 0; i < lines.size(); i++ )
+	{
+		if(reduced_lines.size() == 0)
+		{
+			reduced_lines.push_back(lines[i]);
+		}
+		else
+		{
+			bool found_same = false;
+			for( int j = 0; j < lines.size(); j++ )
+			{
+				// lines with approximately equal end points and start
+				// points will be removed
+				for( int p = 0; p < 2; p++ ){
+					for( int l = 0; l < 2; l++ ){
+						// if points are too close
+						if(distance_point_point(lines[i][2*p], lines[i][2*p+1], reduced_lines[j][2*l], reduced_lines[j][2*l+1]) < LINE_CONN_THERSHOLD){
+							if(distance_point_point(lines[i][2*l], lines[i][2*l+1], reduced_lines[j][2*p], reduced_lines[j][2*p+1]) < LINE_CONN_THERSHOLD){
+							// if already stored line is longer than the current, keep it and discard the current
+							double stored_line_l = distance_point_point(lines[j][0], lines[j][1], reduced_lines[j][2], reduced_lines[j][3]);
+							double current_line_l = distance_point_point(lines[i][0], lines[i][1], reduced_lines[i][2], reduced_lines[i][3]);
+							found_same = true;
+							}
+						}
+					}
+				}
+			}
+			if (found_same == false){
+				reduced_lines.push_back(lines[i]);
+			}
+		}
+	}
+	
+	return reduced_lines;
 }
