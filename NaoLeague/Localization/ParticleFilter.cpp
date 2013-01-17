a1596a33f9023fdd18256f159f0dfdb24ad85615
fleischhauf <fleischhauf@hotmail.com>
2013-01-17 22:00:13 +0100

    apparently something went wrong, rewritten code for vectors again..

--- NaoLeague/Localization/ParticleFilter.cpp
+++ NaoLeague/Localization/ParticleFilter.cpp
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <assert.h>
 #include <time.h>
+#include <vector>
 #define PI 3.14159
 
 using namespace std;
@@ -34,32 +35,16 @@ double ParticleFilter::random_gaussian ()
 ParticleFilter::ParticleFilter(){
 	//random seed:
 	srand(time(NULL));
-
-	this->width = 740;
-	this->height = 540;
-	this->particle_head = 0;
-	this->particle_current = 0;
-	this->particle_count = 0;
-	this->alpha1 = 0.1;
-	this->alpha2 = 0.01;
-	this->alpha3 = 0.01;
-	this->alpha4 = 0.1;
-	//create_particles(150);
+	this->x_dim = 740;
+	this->y_dim = 540;
 }
 ParticleFilter::ParticleFilter(int width, int height,int number_of_particles){
 	//random seed:
 	srand(time(0));
 
-	this->width = width;
-	this->height = height;
-	this->particle_head = 0;
-	this->particle_current = 0;
-	this->particle_count = 0;
-	this->alpha1 = 0.1;
-	this->alpha2 = 0.01;
-	this->alpha3 = 0.01;
-	this->alpha4 = 0.1;
-	create_particles(number_of_particles);
+	this->x_dim = 740;
+	this->y_dim = 540;
+
 }
 ParticleFilter::~ParticleFilter(){
 
@@ -73,31 +58,8 @@ void ParticleFilter::test() {
 /*
  * deletes the linked list of particles and all its descendants
  */
-int ParticleFilter::delete_particle_list(Particle* particle_ptr){
-	//cout<<"deleting particles"<<endl;
-	Particle* particle_del = particle_ptr;
-	this->particle_head = 0;
-	int no = 0;
-	while(particle_ptr != 0)
-	{
-		//delete praticle
-		particle_ptr = particle_ptr->next;
+int ParticleFilter::delete_particles(){
 
-		//cout<<"particle_ptr = particle_ptr->next"<<endl;
-
-		particle_del = particle_ptr;
-		//cout<<"particle_del = particle_ptr "<<no<<endl;
-
-		if(particle_del !=0)
-		{
-			particle_del->~Particle();
-			this->particle_count--;
-			//cout<<"particle_del->~Particle()"<<endl;
-		}
-
-		no++;
-	}
-	//cout<<"deleted particles"<<endl;
 }
 /*
  * resamples all particles with low variance resampling algorithm. (creates new particle list)
@@ -118,16 +80,28 @@ int ParticleFilter::resample() {
 	// read particle 0..M
 	// draw with probability alpha w[i]t
 	// add x[i]_t to Xt
+
+
+	double bin_border = 0;
+	double weight_sum = this->particles[0];
+
+	double step_size = sum_particles()/(double) this->particles.size();
+
+	double offset = rand() % (int)(step_size*1000)/(double)1000;
+	/*
+	j = 0;
+	for(int i = 0; i < this->particles.size(); i++){
+		weight_sum =  offset + (i-1) *
+	}*/
 }
 int ParticleFilter::dynamic(OdometryInformation odo_inf){
-	this->particle_current = this->particle_head;
 
 	//get odometry information //TODO: check if correct
 
-	while(this->particle_current != 0)
+	for(int i = 0; i < this->particles.size(); i++)
 	{
-		sample_motion_model_simple(odo_inf,this->particle_current);
-		this->particle_current = this->particle_current->next;
+		sample_motion_model_simple(odo_inf,&this->particles[i]);
+
 	}
 	//for 0..M
 		// pose = sample_sample_motion_model
@@ -172,69 +146,18 @@ double ParticleFilter::sample_normal_distribution(double variance){
  */
 int ParticleFilter::sample_motion_model_simple(OdometryInformation odometry_information,Particle* last_pose)
 {
-	//cout<<"sampling from motion model simple"<<endl;
 
-
-	//noise 0.01 meter per 1 meter and 0.2 rad per 2 pi rad? (taken form old python codebase)
-	/*
-	double odometry_noise_x = odometry_information.x + odometry_information.x * (double)sample_normal_distribution(10)/1000.0;
-
-	double odometry_noise_y = odometry_information.y + odometry_information.y * (double)sample_normal_distribution(10)/1000.0;
-
-	double odometry_noise_rot = odometry_information.rot + odometry_information.rot * (double)sample_normal_distribution(10)/50.0;
-
-	cout<<"odometry noise x,y,rot"<<odometry_noise_x<< " "<< odometry_noise_y<<" " << odometry_noise_rot<< endl;
-	last_pose->x = last_pose->x +  odometry_noise_x * cos(last_pose->rot) + odometry_noise_y * sin(odometry_information.rot);
-	last_pose->y = last_pose->y + odometry_noise_y * sin(last_pose->rot) -  odometry_noise_y * cos(odometry_information.rot);
-	last_pose->rot = last_pose->rot +  odometry_noise_rot;
-*/
 	double error_range = 0.01;
 	double error_bearing = 0.2;
 	double error_x = odometry_information.x * this->random_gaussian()* error_range;
 	double error_y = odometry_information.y * this->random_gaussian()* error_range;
 	double error_rot = odometry_information.rot * this->random_gaussian() * error_bearing;
 
-	cout<<"noise "<< error_x<< " " << error_y << " " <<error_rot<<endl;
-
 	last_pose->x = last_pose->x+odometry_information.x + error_x;
 	last_pose->y = last_pose->y+odometry_information.y + error_y;
 	last_pose->rot = last_pose->rot + odometry_information.rot + error_rot;
 
-	//TODO: reject particles outside the field!!
-
-	//cout<<"noise rot "<<odometry_noise_rot<<" "<<last_pose->rot<<"    x "<<odometry_noise_x<<" "<<last_pose->rot<<"   y "<<odometry_noise_y<<last_pose->y<<endl;
-	//cout<<"final "<<last_pose->x<< " "<< last_pose->y<<" "<<last_pose->rot<<endl;
-
-}
-//motion model with odometry (xt-1,xt)T and last pose (not working at the moment!)
-int ParticleFilter::sample_motion_model(OdometryInformation odometry_information,Particle* last_pose)
-{
-	//Fox et al probabilistic robotics 136
-	//last_pose.x = odometry_information.x + random_gaussian();
-	//last_pose.y = odometry_information.y + random_gaussian();
-	//last_pose.rot = odometry_information.rot + random_gaussian();
-
-
-	//assuming last pose is identical to old odometry information.
-	double delta_x = odometry_information.x - last_pose->x;
-	double delta_y = odometry_information.y - last_pose->y;
-
-	double delta_rot1 = atan2(delta_x,delta_y)-last_pose->rot;
-	double delta_trans = sqrt( delta_x * delta_x + delta_y * delta_y);
-	double delta_rot2 = odometry_information.rot - last_pose->x - delta_rot1;
-
-	double delta_rot1_err = delta_rot1 - sample_normal_distribution(this->alpha1 * delta_rot1 * delta_rot1 + alpha2 * delta_trans * delta_trans);
-	double delta_trans_err = delta_trans - sample_normal_distribution(this->alpha3 * delta_trans * delta_trans + this->alpha4 * delta_rot1 * delta_rot1 + this->alpha4 * delta_rot2 * delta_rot2);
-	double delta_rot2_err = delta_rot2 - sample_normal_distribution(this->alpha1 * delta_rot2 * delta_rot2 + this->alpha2 * delta_trans * delta_trans);
-
-
-	//cout<<delta_rot1<< " "<<delta_rot2 <<" "<<delta_trans<<endl;
-	//cout<<"odo:"<<odometry_information.x<<" "<<odometry_information.y<<endl;
-
-	//TODO: might have to return pointer instead of object!
-	last_pose->x = last_pose->x + delta_trans_err * cos(last_pose->rot + delta_rot1_err);
-	last_pose->y = last_pose->y + delta_trans_err * sin(last_pose->rot + delta_rot1_err);
-	last_pose->rot = last_pose->rot + delta_rot1_err + delta_rot2_err;
+	//TODO: reject particles outside the field
 }
 
 /*
@@ -320,82 +243,51 @@ double ParticleFilter::measurement_model(VisualFeature* feature,int no_observati
 /*
  * creates a number of particles with fixed poses:
  */
-int ParticleFilter::create_particles(int number,double x, double y, double rot, double weight){
+int ParticleFilter::add_particles(int number,double x, double y, double rot, double weight){
 	//determines how accurate we want the particles to be. cm/accuracy
 
-	assert(this->particle_count == 0);
-
-	Particle* head_node = new Particle(x,y,rot,weight);
-	this->particle_head = head_node;
-	this->particle_current = head_node;
-	this->particle_count++;
-	Particle* current = this->particle_head;
-	//create random samples
-	for (int i = 1; i<number; i++){
-		Particle* new_node = new Particle(x,y,rot,weight);
-
-		current->next = new_node;
-
-		current = new_node;
-		this->particle_count++;
+	for (int i = 0; i< number; i++){
+		Particle p;
+		p.x = x;
+		p.y = y;
+		p.rot = rot;
+		p.weight = 1;
+		this->particles.push_back(p);
 	}
 
 }
 /*
  * creates a number of particles with random poses
  */
-int ParticleFilter::create_particles(int number) {
-	assert(this->particle_count == 0);
+int ParticleFilter::add_particles(int number) {
 	//determines how accurate we want the particles to be. cm/accuracy
 	double accuracy = 10;
 
-	double x = (rand()%(int)(this->width*(int)accuracy))/accuracy;
-	double y = (rand()%(int)(this->height*accuracy))/accuracy;
-	double rot = (rand()%(int)(2*PI*accuracy))/accuracy-PI;//rand()%630;
-	double weight = 1;
-	Particle* head_node = new Particle(x,y,rot,weight);
-	this->particle_head = head_node;
-	this->particle_current = head_node;
-	this->particle_count++;
-	Particle* current = this->particle_head;
-
-
 	//create random samples
 	for (int i = 1; i<number; i++){
-		double x  = (rand()%(int)(this->width*accuracy))/accuracy;
-		double y = (rand()%(int)(this->height*accuracy))/accuracy;
+		double x  = (rand()%(int)(this->x_dim*accuracy))/accuracy - this->x_dim/2;
+		double y = (rand()%(int)(this->y_dim*accuracy))/accuracy - this->y_dim/2;
 		double rot = (rand()%(int)(2*PI*accuracy))/accuracy-PI;//rand() % 630;
 		double weight = 1;
-		Particle* new_node = new Particle(x,y,rot,weight);
-		current->next = new_node;
-		current = current->next;
-		this->particle_count++;
+		Particle p(x,y,rot,weight);
+		this->particles.push_back(p);
 	}
 }
 
-int ParticleFilter::count_particles() {
-
-	Particle* ptr = this->particle_head;
-
-	cout<<"entered_coutnparticlese"<<endl;
-	int count = 0;
-	while(ptr != 0){
-		ptr = ptr->next;
-		count++;
-		cout<<count;
-		cout<<"in while loop!"<<endl;
+double ParticleFilter::sum_weights(){
+	double sum = 0;
+	for(int i = 0; i < this->particles.size(); i++ ){
+		sum += this->particles[i];
 	}
-	cout<<"exiting countparticles"<<endl;
-	return (count);
+	return(sum);
 }
-
-
+/*
+ * prints out values for every particle in particle vector
+ */
 int ParticleFilter::print_particles() {
-	Particle* ptr = this->particle_head;
-	int ctr = 0;
-	while(ptr!=0){
-		cout<<"particle "<<ctr<<" x="<<ptr->x<<" y="<<ptr->y<<" rot="<<ptr->rot<<endl;
-		ptr = ptr->next;
-		ctr++;
+	cout<<"contents of particle list: x y rot weight"<<endl;
+	for(int i = 0; i < this->particles.size(); i++){
+		cout<<this->particles[i].x<<" "<<this->particles[i].y<<" "<<this->particles[i].rot<<" "<<this->particles[i].weight<<endl;
 	}
+
 }
