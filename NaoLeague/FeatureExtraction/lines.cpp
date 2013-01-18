867085c928885980750d1ee1be9c1c2db89c708b
GiorgosMethe <giorgosmethe@gmail.com>
2013-01-18 19:08:51 +0100

    line clustering works with similarity measure the angle between the lines, we will add distances to have a more robust clustering

--- NaoLeague/FeatureExtraction/lines.cpp
+++ NaoLeague/FeatureExtraction/lines.cpp
@@ -14,13 +14,13 @@ double line_angle(Vec4i line)
 	angle = angle * (180 / CV_PI);
 	if( angle < 0 )
 		angle += 180;
-	cout << angle << endl;
 	return angle;
 }
 
 
 double similarity_measure(Vec4i line1, Vec4i line2)
 {
+	return abs(line_angle(line1) - line_angle(line2));
 }
 
 void initialize_clusters(vector<Vec4i> lines, vector< vector<Vec4i> > &cluster)
@@ -44,6 +44,57 @@ void line_clustering(Mat image, vector<Vec4i> lines, vector<Vec4i> &clustered_li
 		// create clusters
 		vector< vector<Vec4i> > cluster;
 		initialize_clusters(lines, cluster);
+		Mat black = Mat::zeros(image.rows, image.cols, CV_8UC3);
+
+		bool end = false;
+
+		do
+		{
+			end = true;
+			for( int i  = 0; i < cluster.size(); i++)
+			{
+				for( int j  = 0; j < cluster.size(); j++)
+				{
+					if( i != j)
+					{
+						for( int ii  = 0; ii < cluster[i].size(); ii++)
+						{
+							for( int jj  = 0; jj < cluster[j].size(); jj++)
+							{
+								if( similarity_measure(cluster[i][ii], cluster[j][jj]) < 10)
+								{
+									end = false;
+									cluster[i].push_back(cluster[j][jj]);
+									cluster[j].erase(cluster[j].begin() + jj);									
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		while(!end);
+
+		for( int i  = 0; i < cluster.size(); i++)
+		{
+			if(cluster.size() > 0)
+			{
+			Mat temp;
+			black.copyTo(temp);
+			for( int j  = 0; j < cluster[i].size(); j++)
+			{
+				line( temp, Point(cluster[i][j][0], cluster[i][j][1]),
+			      Point(cluster[i][j][2], cluster[i][j][3]), Scalar(0,0,255), 1, 8 );
+			}
+			
+			stringstream ss;
+			ss << i;
+			imshow("cluster"+ss.str(),temp);
+			cout << "cluster" << i << " " << cluster[i].size() << endl;
+			}
+			
+			
+		}
 
 		Mat field = Mat::zeros(image.rows, image.cols, CV_8UC3);
 
