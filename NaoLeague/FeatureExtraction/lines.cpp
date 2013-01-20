a1990385164e640e3d872850d4af1a7ec997e3f2
GiorgosMethe <giorgosmethe@gmail.com>
2013-01-20 20:24:38 +0100

    a new feature for similarity measure added, seems to cluster better the lines

--- NaoLeague/FeatureExtraction/lines.cpp
+++ NaoLeague/FeatureExtraction/lines.cpp
@@ -10,41 +10,6 @@ using namespace std;
 
 #define ANGLE_THR 10
 
-double line_angle(Vec4i line)
-{
-	double angle = atan2(line[2]-line[0],line[3]-line[1]);
-	angle = angle * (180 / CV_PI);
-	if( angle < 0 )
-		angle += 180;
-	return angle;
-}
-
-Point line_center(Vec4i line)
-{
-	int x = line[0] + line[2];
-	int y = line[1] + line[3];
-	return Point(x/2,y/2);	
-}
-
-double points_distance(Point point1, Point point2)
-{
-	double dx = point2.x - point1.x;
-	double dy = point2.y - point1.y;
-	double distance = sqrt(dx*dx + dy*dy);
-	return distance;
-}
-
-double point_line_distance(Point point, Vec4i line)
-{
-	double normalLength = hypot(line[2] - line[0], line[3] - line[1]);
- 	return abs((point.x - line[0]) * (line[3] - line[1]) - (point.y - line[1]) * (line[2] - line[0])) / normalLength;
-}
-
-Point middle_point_line(Vec4i line){
-	double x = (line[0] + line[2])/2;
-	double y = (line[1] + line[3])/2;
-	return Point(floor(x),floor(y));
-}
 
 double compute_white_ratio(Mat image, Point point1, Point point2){
 
@@ -85,33 +50,93 @@ double compute_white_ratio(Mat image, Point point1, Point point2){
 		   err = err - dy;
 		   x0 = x0 + sx;
 		}
-		if(e2 <  dx){ 
+		if(e2 <  dx){
 		   err = err + dx;
 		   y0 = y0 + sy;
 		}
 	}
-	return (white_counter/all_counter);
+	cout << "w " << white_counter << endl;
+	cout << "a " << all_counter << endl;
+	return white_counter/all_counter;
+}
+
+double line_angle(Vec4i line)
+{
+	double angle = atan2(line[2]-line[0],line[3]-line[1]);
+	angle = angle * (180 / CV_PI);
+	if( angle < 0 )
+		angle += 180;
+	return angle;
+}
+
+Point line_center(Vec4i line)
+{
+	int x = line[0] + line[2];
+	int y = line[1] + line[3];
+	return Point(x/2,y/2);	
+}
+
+double points_distance(Point point1, Point point2)
+{
+	double dx = point2.x - point1.x;
+	double dy = point2.y - point1.y;
+	double distance = sqrt(dx*dx + dy*dy);
+	return distance;
+}
+
+double point_line_distance(Point point, Vec4i line)
+{
+	double normalLength = hypot(line[2] - line[0], line[3] - line[1]);
+ 	return abs((point.x - line[0]) * (line[3] - line[1]) - (point.y - line[1]) * (line[2] - line[0])) / normalLength;
 }
 
 // measure which is used in order to find clusters that contain
 // similar lines which are propably belong into the same big line
-double similarity_measure(Mat image, vector<Vec4i> cluster1, vector<Vec4i> cluster2)
+double similarity_measure(vector<Vec4i> cluster1, vector<Vec4i> cluster2)
 {
 	double similarity = 0;
 	double min_angle_diff = DBL_MAX;
-	double points_distance = 0;
 	for( int i=0; i < cluster1.size(); i++){
 		for( int j=0; j < cluster2.size(); j++){
-			// angle difference between two lines of the two
-			// compared clusters
+			
+			double max_length = 0;
+			int k1 = 1;
+			int p1 = 1;
+			double angle1;
+			double angle2;
+			for( int k  = 0; k < 2; k++)
+			{
+				k1 = 1-k;
+				for( int p  = 0; p < 2; p++)
+				{
+					p1 = 1-p;
+					if(points_distance(Point(cluster1[i][2*k],cluster1[i][2*k+1]),Point(cluster2[j][2*p],cluster2[j][2*p+1])) > max_length){
+						max_length = points_distance(Point(cluster1[i][2*k],cluster1[i][2*k+1]),Point(cluster2[j][2*p],cluster2[j][2*p+1]));
+						angle1 = line_angle(Vec4i(cluster1[i][2*k],cluster1[i][2*k+1],cluster2[j][2*p],cluster2[j][2*p+1]));
+						angle2 = line_angle(Vec4i(cluster1[i][2*k],cluster1[i][2*k+1],cluster2[j][2*p],cluster2[j][2*p+1]));
+						double line_a = line_angle(cluster1[i]);
+						angle1 = abs(line_a - angle1);
+						angle2 = abs(line_a - angle2);
+
+					}
+				}
+			}
+			similarity += angle1 + angle2;
+
+			// for(int ii=0; ii < 2; ii ++){
+			// 	points_distance += point_line_distance(Point(cluster2[j][0], cluster2[j][1]), cluster1[i]) + 
+			// 	point_line_distance(Point(cluster2[j][2], cluster2[j][3]), cluster1[i]);
+			// }
+			// // angle difference between two lines of the two
+			// // compared clusters
 			double angle_diff = abs(line_angle(cluster1[i]) - line_angle(cluster2[j]));
 			if(angle_diff <  min_angle_diff){
 				min_angle_diff = angle_diff;
 			}
 		}
 	}
-
-	return min_angle_diff;
+	cout << similarity << endl;
+	return similarity+min_angle_diff;
 }
 
 void initialize_clusters(vector<Vec4i> lines, vector< vector<Vec4i> > &cluster)
@@ -131,7 +156,6 @@ void merge_clusters(vector<Vec4i> &cluster_dst, vector<Vec4i> &cluster_src)
 	for(int i=0; i<cluster_src.size(); i++){
 		cluster_dst.push_back(cluster_src[i]);
 	}
-
 	for(int j=0; j<cluster_src.size(); j++){
 		cluster_src.erase(cluster_src.begin() + j);
 	}
@@ -140,15 +164,13 @@ void merge_clusters(vector<Vec4i> &cluster_dst, vector<Vec4i> &cluster_src)
 
 void line_clustering(Mat image, vector<Vec4i> lines, vector<Vec4i> &clustered_lines)
 {
-	if(lines.size() > 2)
+	if(lines.size() > 1)
 	{
 		// create clusters
 		vector< vector<Vec4i> > cluster;
 		initialize_clusters(lines, cluster);
 		Mat black = Mat::zeros(image.rows, image.cols, CV_8UC3);
-		imshow("image", image);
 
-		// merge clusters according to their angle
 		double min_similarity;
 		int cluster_src;
 		int cluster_dst;
@@ -161,7 +183,7 @@ void line_clustering(Mat image, vector<Vec4i> lines, vector<Vec4i> &clustered_li
 				{
 					if( i != j)
 					{
-						double similarity = similarity_measure(image, cluster[i], cluster[j]);
+						double similarity = similarity_measure(cluster[i], cluster[j]);
 						if( similarity < min_similarity)
 						{
 							min_similarity = similarity;
@@ -176,15 +198,7 @@ void line_clustering(Mat image, vector<Vec4i> lines, vector<Vec4i> &clustered_li
 			if(cluster.size() == 1)
 				break;
 		}
-		while(min_similarity < 2);
-
-		// for( int i  = 0; i < cluster.size(); i++)
-		// {
-		// 	for( int j  = 0; j < cluster[i].size(); j++)
-		// 	{
-		// 		for()
-		// 	}
-		// }
+		while(min_similarity < 30);
 
 		if(cluster.size() > 0)
 		{
